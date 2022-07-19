a5988082648790eb38343535a05095d27bda24d8
Jacob Dev <mercihabam@gmail.com>
2022-07-19 18:42:20 +0200

    add succes component

---
+++ src/Screens/EventDetail/components/Checkout/components/SuccessModal/index.jsx
@@ -0,0 +1,40 @@
+import { View, Text, StyleSheet, Image } from 'react-native'
+import React from 'react'
+import { Modal } from 'native-base';
+import vector from '../../../../../../Assets/vectors/checked.png';
+
+const SuccessModal = ({showModal, onClose}) => {
+  return (
+    <Modal isOpen={showModal} onClose={onClose}>
+        <Modal.Content w="full" h="full">
+          <Modal.CloseButton />
+            <Modal.Body style={styles.container}>
+                <Image source={vector} alt="sucess" style={{width: 200, height: 200}} />
+                <Text style={styles.msg}>
+                    Votre demande a été traitée avec succès. Vous allez recevoir une notification avec le résultat.
+                </Text>
+            </Modal.Body>
+        </Modal.Content>
+      </Modal>
+  )
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 20,
+        position: 'absolute',
+        top: 100,
+        left: 50,
+        right: 50
+    },
+    msg: {
+        fontFamily: 'Barlow-Bold',
+        textAlign: 'center',
+        marginTop: 10
+    }
+})
+
+export default SuccessModal
--- src/Screens/EventDetail/components/Checkout/index.jsx
+++ src/Screens/EventDetail/components/Checkout/index.jsx
@@ -9,6 +9,7 @@ import axios from 'axios'
 import { showToast } from '../../../../Utils/feedbacks';
 import * as yup from 'yup';
 import { useFocusEffect } from '@react-navigation/native'
+import SuccessModal from './components/SuccessModal'
 
 const validationSchema = yup.object({
     phone_number: yup.string().required('Ce champs est obligatoire')
@@ -16,6 +17,7 @@ const validationSchema = yup.object({
 
 const Checkout = ({isOpen, setIsOpen, ticket}) => {
     const [loading, setLoading] = useState(false);
+    const [showSucces, setShowSucess] = useState(false);
     const { setFieldValue, values, handleSubmit, errors } = useFormik({
         initialValues: {
             phone_number: ''
@@ -25,6 +27,8 @@ const Checkout = ({isOpen, setIsOpen, ticket}) => {
             const phone = values.phone_number.replace(/\D/g, '');
             try {
                 const res = await axios.post(`/api/v1/payments/initiate`, { amount: ticket.price, currency: ticket.currency, phone, ticket: ticket.id, event: ticket.event?.id });
+                setShowSucess(true);
+                setIsOpen(false);
             } catch (error) {
                 const response = error.response;
                 if(response){
@@ -53,48 +57,51 @@ const Checkout = ({isOpen, setIsOpen, ticket}) => {
     )
 
   return (
-    <Actionsheet isOpen={isOpen} onClose={() =>setIsOpen(false)}>
-        <Actionsheet.Content p={5} bg="white">
-        <KeyboardAwareScrollView>
-        <View style={styles.flexFields}>
-            <CommonInput 
-                label="Prix"
-                kType='numeric'
-                style={{
-                    width: '50%'
-                }}
-                value={ticket['price']}
-                editable={false}
-                isDisabled={true}
-            />
-            <CommonSelect label="Devise" 
-                required
-                uiType="rounded"
-                style={{
-                    width: '40%'
-                }}
-                value={ticket['currency']}
-                isDisabled={true}
+    <>
+        <SuccessModal showModal={showSucces} onClose={() =>{ setShowSucess(false); setIsOpen(false) }} />
+        <Actionsheet isOpen={isOpen} onClose={() =>setIsOpen(false)}>
+            <Actionsheet.Content p={5} bg="white">
+            <KeyboardAwareScrollView>
+            <View style={styles.flexFields}>
+                <CommonInput 
+                    label="Prix"
+                    kType='numeric'
+                    style={{
+                        width: '50%'
+                    }}
+                    value={ticket['price']}
+                    editable={false}
+                    isDisabled={true}
+                />
+                <CommonSelect label="Devise" 
+                    required
+                    uiType="rounded"
+                    style={{
+                        width: '40%'
+                    }}
+                    value={ticket['currency']}
+                    isDisabled={true}
+                >
+                    <Select.Item label='$' value='usd' />
+                    <Select.Item label='FC' value='cdf' />
+                </CommonSelect>
+            </View>
+            <View style={styles.wallet}>
+                <FormControl.Label style={styles.label}>Entrer le numéro mobile-money</FormControl.Label>
+                <CommonPhoneInput value={values.phone_number} setFieldValue={setFieldValue} required={true} error={errors.phone_number} />
+            </View>
+            <Button
+                isLoadingText="Patientez..."
+                style={styles.btn}
+                isLoading={loading}
+                onPress={handleSubmit}
             >
-                <Select.Item label='$' value='usd' />
-                <Select.Item label='FC' value='cdf' />
-            </CommonSelect>
-        </View>
-        <View style={styles.wallet}>
-            <FormControl.Label style={styles.label}>Entrer le numéro mobile-money</FormControl.Label>
-            <CommonPhoneInput value={values.phone_number} setFieldValue={setFieldValue} required={true} error={errors.phone_number} />
-        </View>
-        <Button
-            isLoadingText="Patientez..."
-            style={styles.btn}
-            isLoading={loading}
-            onPress={handleSubmit}
-        >
-            Payer
-        </Button>
-        </KeyboardAwareScrollView>
-        </Actionsheet.Content>
-    </Actionsheet>
+                Payer
+            </Button>
+            </KeyboardAwareScrollView>
+            </Actionsheet.Content>
+        </Actionsheet>
+    </>
   )
 }
 
